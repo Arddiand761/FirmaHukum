11109dd7c600bf000ef019b7104821f30eab5200
Arddian Darmawan_2200018157 <128204076+Arddiand761@users.noreply.github.com>
2025-04-26 22:52:48 +0700

    Penambahan Bootstrap
    membuat landing page

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,53 @@
+'use strict';
 const path = require('path');
+const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
- 
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+
+
+  mode: 'production',
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+
+  
   module: {
     rules: [
       /* style and css loader */
       {
-        test: /\.css$/,
+        test: /\.(scss)$/,
         use: [
+          'style-loader',
+          'css-loader',
           {
-            loader: 'style-loader',
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('autoprefixer')]
+              }
+            }
           },
           {
-            loader: 'css-loader',
-          },
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'), // Gunakan Dart Sass
+              sassOptions: {
+                quietDeps: true // Suppress deprecation warnings
+              }
+            }
+          }
         ],
       },
     ],
